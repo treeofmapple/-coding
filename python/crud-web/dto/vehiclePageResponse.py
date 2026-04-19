5abefb00f14b90314415168f080d884440938c64
pogeku <pogeku@lapiz>
2026-04-19 06:27:38 -0300

    Fixing some folders and old projects

--- python/crud-web/dto/vehiclePageResponse.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-from .vehicleResponse import VehicleResponse
-
-class PageVehicleResponse(BaseModel):
-    content: list[VehicleResponse]
-    page: int
-    size: int
-    total_pages: int
--- python/crud-web/dto/vehicleUpdate.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class VehicleUpdate(BaseModel):
-    brand: str | None = None
-    model: str | None = None
-    color: str | None = None
-    plate: str | None = None
